19aade913ab68013428ee3b04ac17d0f9ce32a79
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-19 20:24:50 +0800

    Update utils.js

--- modules/utils.js
+++ modules/utils.js
@@ -1,68 +1,22 @@
-const fs = require("fs-extra");
+const config = require("../config.json");
+const theme = require("../website/web.js").getTheme();
 
 function getEventType(event) {
   return new Promise((resolve) => {
     let type = "unknown";
-
-    if (event) {
-      const msg = event.message;
-      if (msg) {
-        if (msg.attachments) {
-          type = "attachments";
-        } else if (msg.reply_to) {
-          type = "message_reply";
-        } else if (msg.quick_reply) {
-          type = "quick_reply";
-        } else {
-          type = "message";
-        }
-      } else if (event.response_feedback) {
-        type = "response_feedback";
-      } else if (event.postback) {
-        type = "postback";
-      } else if (event.reaction) {
-        type = "message_reaction";
-      } else if (event.read) {
-        type = "mark_as_read";
-      } else if (event.delivery) {
-        type = "mark_as_delivered";
-      }
-    }
-
+    if (event.message) {
+      if (event.message.attachments) type = "attachments";
+      else if (event.message.reply_to) type = "message_reply";
+      else type = "message";
+    } else if (event.postback) type = "postback";
     resolve(type);
   });
 }
 
-async function log(event) {
-  const config = JSON.parse(fs.readFileSync("./config.json"), "utf8");
-  let senderId = event.sender.id || null;
-
-  if (config.ADMINS.includes(senderId)) {
-    piece = "ADMIN";
-  } else {
-    piece = "USER";
-  };
-
-  const theme = require('../website/web.js').getTheme();
-
-  if (event?.message?.text && !event?.message?.is_echo) {
-    console.log(`${theme.gradient.multiline(piece)}: ${event.message.text} ${theme.color((await getEventType(event)).toUpperCase())}`)
-  } else if (event.type === "message_reaction") {
-    if (event.reaction.emoji) {
-       console.log(`${theme.gradient.multiline(piece)}: ${senderId} reacted "${event.reaction.emoji}" to a message. ${theme.color((await getEventType(event)).toUpperCase())}`)
-    } else {
-       console.log(`${theme.gradient.multiline(piece)}: ${senderId} removed a reaction to a message. ${theme.color((await getEventType(event)).toUpperCase())}`)
-    }
-  } else if (event?.message?.is_echo && !config.selfListen) {
-    console.log(`${theme.gradient.multiline("BOT")}: ${event?.message?.text || event?.message?.attachments?.[0].title || event?.message?.attachments[0]?.payload.url || null } ${theme.color((await getEventType(event)).toUpperCase())}`);
-  } else if (event.type === "attachments") {
-    console.log(`${theme.gradient.multiline(piece)}: ${event?.message?.attachments[0]?.payload.url || null} ${theme.color((await getEventType(event)).toUpperCase())}`)
-  } else if (event.type === "postback") {
-    console.log(`${theme.gradient.multiline(piece)}: ${event?.postback?.title} ${theme.color((await getEventType(event)).toUpperCase())}`);
-  }
+function log(event) {
+  let sender = config.ADMINS.includes(event.sender?.id) ? "ADMIN" : "USER";
+  if (event.message?.is_echo) sender = "BOT";
+  console.log(`${theme.gradient.multiline(sender)}: Event Received`);
 }
 
-module.exports = {
-  log,
-  getEventType,
-};
+module.exports = { log, getEventType };
