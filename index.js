91ce012a08e81b4d738acd5bf3cf22d079d7612c
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-19 20:23:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,25 +7,14 @@ const app = express();
 
 app.use(parser.json());
 app.use(express.static("website"));
-app.get("/config.json", (req, res) => {
-  res.sendFile(path.join(__dirname, "config.json"));
-});
 
-app.get("/", (req, res) => {
-  web.html(res);
-});
-
-app.get("/webhook", (req, res) => {
-  web.verify(req, res);
-});
-
-setTimeout(() => {
-  app.post("/webhook", (req, res) => {
+app.get("/", (req, res) => { web.html(res); });
+app.get("/webhook", (req, res) => { web.verify(req, res); });
+app.post("/webhook", (req, res) => {
     webhook.listen(req.body);
     res.sendStatus(200);
-  });
-}, 5000);
+});
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   web.log();
 });
