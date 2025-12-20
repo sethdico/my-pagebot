254960f8e01b9967dfab99dddba9274794e5b70e
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-20 14:18:08 +0800

    Update sendAttachment.js

--- page/src/sendAttachment.js
+++ page/src/sendAttachment.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const FormData = require("form-data");
 const fs = require("fs");
+const { API_VERSION } = require("../../config.json");
 
 module.exports = function (event) {
   return async function sendAttachment(type, source, senderID) {
@@ -11,7 +12,7 @@ module.exports = function (event) {
 
     try {
       if (isUrl) {
-        await axios.post(`https://graph.facebook.com/v21.0/me/messages?access_token=${accessToken}`, {
+        await axios.post(`https://graph.facebook.com/${API_VERSION}/me/messages?access_token=${accessToken}`, {
           recipient: { id: recipientID },
           message: { attachment: { type: mediaType, payload: { url: source, is_reusable: true } } }
         });
@@ -20,7 +21,7 @@ module.exports = function (event) {
         form.append("recipient", JSON.stringify({ id: recipientID }));
         form.append("message", JSON.stringify({ attachment: { type: mediaType, payload: { is_reusable: true } } }));
         form.append("filedata", fs.createReadStream(source));
-        await axios.post(`https://graph.facebook.com/v21.0/me/messages?access_token=${accessToken}`, form, { headers: form.getHeaders() });
+        await axios.post(`https://graph.facebook.com/${API_VERSION}/me/messages?access_token=${accessToken}`, form, { headers: form.getHeaders() });
       }
     } catch (err) { console.error("Attachment Failed", err.message); }
   };
