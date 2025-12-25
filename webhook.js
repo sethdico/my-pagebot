942c4e969c06fc52071b6ba79b6c889f33654c2d
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-26 01:24:52 +0800

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -8,15 +8,18 @@ module.exports.listen = (event) => {
 
   event.entry.forEach((entry) => {
     entry.messaging.forEach(async (ev) => {
+      // block banned users immediately
       if (!ev.sender?.id || (global.BANNED_USERS && global.BANNED_USERS.has(ev.sender.id))) return;
 
       ev.type = await utils.getEventType(ev);
 
+      // context caching
       if (ev.message?.mid) {
         if (messagesCache.size > 1000) messagesCache.delete(messagesCache.keys().next().value);
         messagesCache.set(ev.message.mid, { text: ev.message.text, attachments: ev.message.attachments });
       }
 
+      // reply context
       if (ev.type === "message_reply") {
         const cached = messagesCache.get(ev.message.reply_to?.mid);
         if (cached) {
