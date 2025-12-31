5d1c850f901be43b74b9fe90fc3aef8ae924ab3e
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-31 13:36:07 +0800

    Update maintenance.js

--- modules/scripts/commands/maintenance.js
+++ modules/scripts/commands/maintenance.js
@@ -1,12 +1,12 @@
 const db = require("../../database");
 
 module.exports.config = {
-    name: "maintenance", author: "Sethdico", version: "2.6", category: "Admin", description: "Toggle bot availability.", adminOnly: true, usePrefix: false, cooldown: 0,
+    name: "maintenance", author: "Sethdico", version: "2.7", category: "Admin", description: "Toggle bot availability.", adminOnly: true, usePrefix: false, cooldown: 0,
 };
 
 module.exports.run = async function ({ args, reply }) {
     const action = args[0]?.toLowerCase();
-    const reason = args.slice(1).join(" ") || "The owner is currently updating and fixing the bot.";
+    const reason = args.slice(1).join(" ") || "bot's getting updated rn, back soon";
 
     if (action === "on") {
         global.MAINTENANCE_MODE = true;
