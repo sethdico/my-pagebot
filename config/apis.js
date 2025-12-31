ea7e6489935fcc6a5567325402bbff88f46e6d42
Amadeus <138116141+sethdico@users.noreply.github.com>
2025-12-31 13:13:28 +0800

    Create apis.js

---
+++ config/apis.js
@@ -0,0 +1,10 @@
+module.exports = {
+    aria: "https://betadash-api-swordslush-production.up.railway.app/Aria",
+    brave: "https://betadash-api-swordslush-production.up.railway.app/brave",
+    phind: "https://api.ccprojectsapis-jonell.gleeze.com/api/phindai",
+    quillbot: "https://api.ccprojectsapis-jonell.gleeze.com/api/ai/quillbotai",
+    webpilot: "https://shin-apis.onrender.com/ai/webcopilot",
+    copilot: "https://shin-apis.onrender.com/ai/copilot",
+    you: "https://betadash-api-swordslush-production.up.railway.app/you",
+    deepimg: "https://shin-apis.onrender.com/ai/deepimg"
+};
